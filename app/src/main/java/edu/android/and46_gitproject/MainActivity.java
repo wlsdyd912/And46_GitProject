5aaf60f693f7ccb309daf96240d1378d73afdd38
wlsdyd912 <44281897+wlsdyd912@users.noreply.github.com>
2018-10-19 14:27:34 +0900

    수정함..

--- app/src/main/java/edu/android/and46_gitproject/MainActivity.java
+++ app/src/main/java/edu/android/and46_gitproject/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-
+        textView.setText("안녕하세요");
+        
     }
 }
